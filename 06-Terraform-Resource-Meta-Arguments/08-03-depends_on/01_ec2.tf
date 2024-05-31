0da19832a8cca77998505b8dab15dcbeb3216c70
Joyous <62016480+JoyousKonyana@users.noreply.github.com>
2024-05-31 07:13:27 +0200

    Delete 06-Terraform-Resource-Meta-Arguments directory

--- 06-Terraform-Resource-Meta-Arguments/08-03-depends_on/01_ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "my-web-ec2" {
-  ami           = "ami-0df435f331839b2d6"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name  = "linux-webserver"
-    Owner = "Venkatesh"
-  }
-}
